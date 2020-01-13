728c897be6c9090bd1d50f722fa4536e86ce0e15
kryhin <artem@maxtvmedia.com>
2020-01-13 12:10:31 +0200

    Run to collect data

--- Src/Types.go
+++ Src/Types.go
@@ -8,9 +8,10 @@ const DbLogMode = true
 
 var Err error
 var Port string
+
 var ScreenStatUrl string
 
-const Version = "0.0.1"
+const Version = "0.0.2"
 
 var GetMaxTvStatInterval int64
 
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	Src "./src"
+	"./src"
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
@@ -19,28 +19,28 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	Src.Port = os.Getenv("PORT")
-	Src.ScreenStatUrl = os.Getenv("SCREEN_STAT_URL")
-	Src.GetMaxTvStatInterval, _ = strconv.ParseInt(os.Getenv("GET_MAXTV_STAT_INTERVAL_SECOND"), 10, 64)
+	src.Port = os.Getenv("PORT")
+	src.ScreenStatUrl = os.Getenv("SCREEN_STAT_URL")
+	src.GetMaxTvStatInterval, _ = strconv.ParseInt(os.Getenv("GET_MAXTV_STAT_INTERVAL_SECOND"), 10, 64)
 
-	Src.Db, Src.Err = gorm.Open("sqlite3", "screen_stat.db")
-	if Src.Err != nil {
+	src.Db, src.Err = gorm.Open("sqlite3", "screen_stat.db")
+	if src.Err != nil {
 		panic("failed to connect database")
 	}
-	defer Src.Db.Close()
-	Src.Db.LogMode(Src.DbLogMode)
+	defer src.Db.Close()
+	src.Db.LogMode(src.DbLogMode)
 
-	Src.Db.AutoMigrate(&Src.Ad{})
-	Src.Db.AutoMigrate(&Src.Screen{})
-	Src.Db.AutoMigrate(&Src.Building{})
-	Src.Db.AutoMigrate(&Src.Building{})
-	Src.Db.AutoMigrate(&Src.Stat{})
-	Src.Db.AutoMigrate(&Src.StatGetAttempt{})
-	Src.Db.AutoMigrate(&Src.Video{})
-	Src.Db.AutoMigrate(&Src.Spot{})
+	src.Db.AutoMigrate(&src.Ad{})
+	src.Db.AutoMigrate(&src.Screen{})
+	src.Db.AutoMigrate(&src.Building{})
+	src.Db.AutoMigrate(&src.Building{})
+	src.Db.AutoMigrate(&src.Stat{})
+	src.Db.AutoMigrate(&src.StatGetAttempt{})
+	src.Db.AutoMigrate(&src.Video{})
+	src.Db.AutoMigrate(&src.Spot{})
 
 	go func() {
-		Src.DoEvery(time.Duration(Src.GetMaxTvStatInterval)*time.Second, Src.GetScreenStat)
+		src.DoEvery(time.Duration(src.GetMaxTvStatInterval)*time.Second, src.GetScreenStat)
 	}()
 
 	handleHTTP()
@@ -48,11 +48,11 @@ func main() {
 
 func handleHTTP() {
 
-	http.HandleFunc("/get_statistic", Src.GetStatistic)
-	http.HandleFunc("/get_maxtv_statistic", Src.GetMaxTVStatistic)
+	http.HandleFunc("/get_statistic", src.GetStatistic)
+	http.HandleFunc("/get_maxtv_statistic", src.GetMaxTVStatistic)
 
-	fmt.Printf("Starting Server to HANDLE maxtv.tech back end\nPort : " + Src.Port + "\nAPI revision " + Src.Version + "\n\n")
-	if err := http.ListenAndServe(":"+Src.Port, nil); err != nil {
+	fmt.Printf("Starting Server to HANDLE maxtv.tech back end\nPort : " + src.Port + "\nAPI revision " + src.Version + "\n\n")
+	if err := http.ListenAndServe(":"+src.Port, nil); err != nil {
 		log.Fatal(err)
 	}
 }
